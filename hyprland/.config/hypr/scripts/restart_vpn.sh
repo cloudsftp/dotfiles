910030cd1821ccac82a3df4debdb7c589135758d
Fabian Weik <fabian.weik@protonmail.com>
2024-05-08 10:32:10 +0200

    automatically select vpn to toggle when on mittwald laptop

--- hyprland/.config/hypr/scripts/restart_vpn.sh
+++ hyprland/.config/hypr/scripts/restart_vpn.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env sh
 
-if [ $(hostname) = "mittwald" ]; then
-    systemctl --user restart caa
+if [ "$(hostname)" = "mittwald" ]; then
+    function connected_to_network() {
+        id="$1"
+        if [ "$(nmcli -f GENERAL.STATE con show $id)" ]; then
+            echo true
+        fi
+    }
+
+    function connected_to_internal_network() {
+        if [ "$(connected_to_network Hub)" ] || [ "$(connected_to_network Wifi)" ]; then
+            echo true
+        fi
+    }
+
+    if [ "$(connected_to_internal_network)" ]; then
+        systemctl --user restart caa
+    else
+        ~/Projects/NetworkShi/vpn/toggle_vpn.sh
+    fi
 elif command -v mullvad &> /dev/null; then
     mullvad reconnect
 fi

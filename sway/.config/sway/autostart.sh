cda6dec9e0aee93f41f35881c0cebafb4b1bed0c
Fabian Weik <fabian.weik@protonmail.com>
2024-05-16 08:53:45 +0200

    extend autostart in sway

--- sway/.config/sway/autostart.sh
+++ sway/.config/sway/autostart.sh
@@ -2,16 +2,30 @@
 
 waybar &
 
+firefox &
+
+emacsclient --alternate-editor= --create-frame %F & # not starting correctly
+
 case "$(hostname)" in
     workstation | SuseBook)
         hyprpaper &
         ;;
     mittwald)
         hyprpaper -c ~/.config/hypr/hyprpaper-work.conf &
-        swayidle &
+
+        swayidle -w \
+            timeout 300 swaylock \
+            timeout 360 'swaymsg "output * power off"' \
+                resume 'swaymsg "output * power on"' \
+            before-sleep swaylock &
+
+        slack &
+
+        sleep 5
+
+        restart_vpn & # not working properly
+
         ;;
 esac
 
 syncthing --no-browser &> /tmp/syncthing.log &
-
-firefox &

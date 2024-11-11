839abd3a0fc7d81c7db313fbc07e98f59334eda2
Fabian Weik <fabian.weik@protonmail.com>
2024-11-11 05:15:26 +0100

    fraunhofer setup init

--- sway/.config/sway/autostart.sh
+++ sway/.config/sway/autostart.sh
@@ -12,22 +12,9 @@ case "$(hostname)" in
     workstation | susebook)
         hyprpaper &
         ;;
-    mittwald)
+    fraunhofer)
         hyprpaper -c ~/.config/hypr/hyprpaper-work.conf &
-
-        swayidle -w \
-            timeout 300 swaylock \
-            timeout 360 'swaymsg "output * power off"' \
-                resume 'swaymsg "output * power on"' \
-            before-sleep swaylock &
-
-        slack &
-
-        sleep 5
-
-        restart_vpn & # not working properly
-
-        ;;
+	;;
 esac
 
 syncthing --no-browser &> /tmp/syncthing.log &

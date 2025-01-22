815a17100dc3417ac68086298de4d7966dd8d046
Fabian Weik <fabian.weik@protonmail.com>
2025-01-22 09:31:45 +0100

    more reasonable scale for latex previews

--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -6,5 +6,5 @@
 ;; latex preview
 ;; TODO: only on big monitors
 
-(setq custom/latex-preview-scale 3)
+(setq custom/latex-preview-scale 2.5)
 (setq org-format-latex-options (plist-put org-format-latex-options :scale custom/latex-preview-scale))

c716e2f88d9a6f74c2fdccdf9c10223b7ab395ed
Fabian Weik <fabian.weik@protonmail.com>
2025-02-27 08:48:22 +0100

    new resolution at fraunhofer

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -19,8 +19,8 @@
          (cond ((= (display-pixel-width) 1920) 18) ;; Full HD
                ((= (display-pixel-width) 2560) 30) ;; 4K scaled (Xorg x1.5)
                ((= (display-pixel-width) 2048) 12) ;; 2K - figure out when needed
-               ((and (= (display-pixel-width) 3840)
-                     (= (display-pixel-height) 1600)) 26) ;; 4K @ Fraunhofer
+               ((and (= (display-pixel-width) 4800)
+                     (= (display-pixel-height) 2000)) 26) ;; 4K @ Fraunhofer
                ((= (display-pixel-width) 3840) 30))) ;; 4K
         (noto-mono-family "BitstromWera Nerd Font Mono"))
     (setq doom-font (font-spec :family noto-mono-family :size font-size)

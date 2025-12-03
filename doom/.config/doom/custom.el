320bfce3392d18731c133d2f77f6e3d052022d17
Fabian Weik <fabian.weik@protonmail.com>
2025-12-03 09:25:15 +0100

    zathura as first pdf viewer

--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -3,3 +3,19 @@
 
 (setq custom/latex-preview-scale 2.5)
 (setq org-format-latex-options (plist-put org-format-latex-options :scale custom/latex-preview-scale))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   '((output-pdf "Zathura") (output-pdf "PDF Tools")
+     ((output-dvi has-no-display-manager) "dvi2tty")
+     ((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi")
+     (output-html "xdg-open"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
